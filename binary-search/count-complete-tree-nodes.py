dc804f97a0b0acbe4e8ef2bf993516877a6ca3a4
Yvonne Lee <45631531+lyxyvonnelee@users.noreply.github.com>
2025-07-15 17:07:42 -0700

    LC-count-complete-tree-nodes [Runtime: 0 ms; Memory: 23.3 MB]

--- binary-search/count-complete-tree-nodes.py
+++ binary-search/count-complete-tree-nodes.py
@@ -1,24 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def countNodes(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        left, right = root.left, root.right
-        count = 1
-        while left and right:
-            count += 1
-            left = left.left
-            right = right.right
-        if not left and not right:
-            return 2**count-1
-        return self.countNodes(root.left) + self.countNodes(root.right)+1
+
+        def getDepth(node):
+            depth = 0
+            while node:
+                depth += 1
+                node = node.left
+            return depth
+
+        left = getDepth(root.left)
+        right = getDepth(root.right)
+
+        if left == right:
+            return (1 << left) + self.countNodes(root.right)
+        else:
+            return (1 << right) + self.countNodes(root.left)
