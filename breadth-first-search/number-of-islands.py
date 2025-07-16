48295aa51121d1eaf4956fe210445888fd8424d5
Yvonne Lee <45631531+lyxyvonnelee@users.noreply.github.com>
2025-07-15 21:58:13 -0700

    LC-number-of-islands [Runtime: 236 ms; Memory: 20.3 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -1,24 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        count = 0
-        m, n = len(grid), len(grid[0])
+        self.island_count = 0
 
-        def dfs(r, c):
-            if r >= m or c >= n or r < 0 or c < 0 or grid[r][c] == '0':
+        def dfs(row: int, col: int):
+            if (
+                row < 0 or col < 0 or row >= len(grid) or 
+                col >= len(grid[0]) or grid[row][col] == "0"
+            ):
                 return
-            grid[r][c] = '0'
-            dfs(r-1,c)
-            dfs(r+1,c)
-            dfs(r, c-1)
-            dfs(r, c+1)
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
+            grid[row][col] = "0"
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
                     dfs(i, j)
+                    self.island_count += 1
 
-        return count
+        return self.island_count
