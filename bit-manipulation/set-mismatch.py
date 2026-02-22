9bd858aca1ce21114b0fc533a38c7fb052d273ca
Yvonne Lee <45631531+lyxyvonnelee@users.noreply.github.com>
2026-02-21 17:18:01 -0800

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- bit-manipulation/set-mismatch.py
+++ bit-manipulation/set-mismatch.py
@@ -1,5 +1,6 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
         for i in range(len(nums)):
             if nums[i] != i + 1:
                 return [nums[i], i + 1]
