52294a91f36699abb1f0b7df4caf1f99f62d29eb
Yvonne Lee <45631531+lyxyvonnelee@users.noreply.github.com>
2026-02-21 17:25:40 -0800

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- hash-table/set-mismatch.py
+++ hash-table/set-mismatch.py
@@ -3,8 +3,11 @@ class Solution:
         nums.sort()
         missing = 0
         dup = 0
+        flag = True
+
         for i in range(len(nums)):
-            if nums[i] != i + 1:
+            if nums[i] != i + 1 and flag:
+                flag = False
                 missing = i + 1
             if i != 0 and nums[i] == nums[i - 1]:
                 dup = nums[i]
