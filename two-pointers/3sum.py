7b10d637531c2a1f9de7b872b6cd36b0c67abc0e
Yvonne Lee <45631531+lyxyvonnelee@users.noreply.github.com>
2026-03-24 00:02:39 -0700

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -1,28 +1,30 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
+        result = []
 
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
+        if len(nums) == 3 and sum(nums) == 0:
+            return [nums]
 
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        for i in range(len(nums) - 3):
+            if nums[i] > 0 or (i > 0 and nums[i] == nums[i - 1]):
+                break
+            l, r = i + 1, len(nums) - 1
+            target = -nums[i]
+            while l < r:
+                cur = nums[l] + nums[r]
+                if cur == target:
+                    result.append([nums[i], nums[l], nums[r]])
 
-            while left < right:
-                cur_sum = nums[i] + nums[left] + nums[right]
-
-                if cur_sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif cur_sum < 0:
-                    left += 1
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif cur > target:
+                    r -= 1
                 else:
-                    right -= 1
+                    l += 1
 
         return result
